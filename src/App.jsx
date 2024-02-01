40f77b000e8d2b230e3f8e5221025779bf4e8e90
alsendrha <alsendrha@gmail.com>
2024-02-01 17:26:19 +0900

    app.jsx에  주소 공유

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // 아래 주소 사용하시면 됩니다. 저희는 주소뒤에 키워드 부분만 만지면 됩니다!
+  // https://apis.data.go.kr/B551011/KorService1/searchKeyword1?serviceKey=D6HvbqfFj6otDTGY3883h0C51xIplWlMUXEF%2Bl5ZX9DTpTTNODdcI%2F6StO1BbYtjTAtOOKyj25hhnMVj4ASszw%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A&keyword=%EA%B0%95%EC%9B%90&contentTypeId=12
   return (
     <div>
-      <h1>초기화</h1>
+      <h1> 초기화 </h1>
     </div>
   )
 }
