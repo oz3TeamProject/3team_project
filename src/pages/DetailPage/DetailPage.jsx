3e47a394f3e080caaceb9ca8cfeed006028f8ecc
alsendrha <alsendrha@gmail.com>
2024-02-06 15:53:05 +0900

    detailPage 꾸미기 및 searchPage item수 60으로 증가

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,61 +1,103 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import './DetailPage.css'
-import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./DetailPage.css";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 
 const DetailPage = () => {
   const contentTitle = useParams();
   const [tour, setTour] = useState(null);
-
+  const [detailContent, setDetailContent] = useState("");
+  const APIS_KEY =
+    "D6HvbqfFj6otDTGY3883h0C51xIplWlMUXEF%2Bl5ZX9DTpTTNODdcI%2F6StO1BbYtjTAtOOKyj25hhnMVj4ASszw%3D%3D";
+  const itemCount = 1;
   const containerStyle = {
-  width: '700px',
-    height: '400px',
-  margin: 'auto'
-};
-
+    width: "700px",
+    height: "400px",
+    margin: "auto",
+    borderRadius: "10px",
+  };
 
   useEffect(() => {
     async function fetchData() {
-      const newContentTitle = contentTitle.content_title.replace('content_title:', '');
+      const newContentTitle = contentTitle.content_title.replace(
+        "content_title:",
+        ""
+      );
 
       const response = await axios.get(
-        `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?serviceKey=D6HvbqfFj6otDTGY3883h0C51xIplWlMUXEF%2Bl5ZX9DTpTTNODdcI%2F6StO1BbYtjTAtOOKyj25hhnMVj4ASszw%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A&&contentTypeId=12&keyword=${newContentTitle}`
-      )
+        `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?serviceKey=${APIS_KEY}&numOfRows=${itemCount}&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A&&contentTypeId=12&keyword=${newContentTitle}`
+      );
+
       setTour(response.data.response.body.items.item[0]);
+      const contentId = response.data.response.body.items.item[0].contentid;
+      console.log(contentId);
+      const response2 = await axios.get(
+        `https://apis.data.go.kr/B551011/KorService1/detailCommon1?serviceKey=${APIS_KEY}&MobileOS=ETC&MobileApp=AppTest&_type=json&contentId=${contentId}&contentTypeId=12&defaultYN=Y&overviewYN=Y&numOfRows=1&pageNo=1`
+      );
+      setDetailContent(response2.data.response.body.items.item[0].overview);
     }
     fetchData();
-  }, [contentTitle])
+  }, [contentTitle]);
 
   if (tour === null) return null;
 
   return (
-    <section>
-      <div className='detail_data'>
-        <div className='data_info'>
-          <img src={tour.firstimage ? tour.firstimage : 'images/no_img.jpg'} />
-          <div className='data_text'>
-            <h1>{tour.title}</h1>
-            <p>{`주소: ${tour.addr1} ${tour.addr2}`}</p>
-          </div>
-          <br />
-            <LoadScript
-              googleMapsApiKey="AIzaSyAMUowo4dL4hO_oLvHA3CvHyINSAXjUIjI"
-              loadingElement={<div style={{ height: '100%' }} />}
-            >
-              <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={{lat: parseFloat(tour.mapy), lng: parseFloat(tour.mapx)}}
-                zoom={14}
+    <div className="detail_background">
+      <div className="detail_container">
+        <div className="detail_circle">우리나라 관광지</div>
+        <section className="detail_section">
+          <div className="detail_data">
+            <div className="data_info">
+              <h1 className="detail_title">{tour.title}</h1>
+              <img
+                src={tour.firstimage ? tour.firstimage : "images/no_img.jpg"}
+              />
+              <div className="data_text">
+                <p>{`주소: ${tour.addr1} ${tour.addr2}`}</p>
+                <br />
+                <h4>상세정보</h4>
+                <div
+                  style={{ width: "650px", margin: "auto", textAlign: "start" }}
+                >
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: detailContent
+                        .replace(/<br \/>/g, "<br />")
+                        .replace(/\n/g, "<br />"),
+                    }}
+                  ></p>
+                  {/* <p>{detailContent.replace('<br />', '')}</p> */}
+                </div>
+              </div>
+              <br />
+              <LoadScript
+                googleMapsApiKey="AIzaSyAMUowo4dL4hO_oLvHA3CvHyINSAXjUIjI"
+                loadingElement={<div style={{ height: "100%" }} />}
               >
-                <MarkerF position={{lat: parseFloat(tour.mapy), lng: parseFloat(tour.mapx)}}/>
-              </GoogleMap>
-            </LoadScript>
-          <br />
+                <GoogleMap
+                  mapContainerStyle={containerStyle}
+                  center={{
+                    lat: parseFloat(tour.mapy),
+                    lng: parseFloat(tour.mapx),
+                  }}
+                  zoom={14}
+                >
+                  <MarkerF
+                    position={{
+                      lat: parseFloat(tour.mapy),
+                      lng: parseFloat(tour.mapx),
+                    }}
+                  />
+                </GoogleMap>
+              </LoadScript>
+              <br />
+            </div>
           </div>
+        </section>
       </div>
-    </section>
-  )
-}
+    </div>
+  );
+};
 
-export default DetailPage
+export default DetailPage;
--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,8 +1,8 @@
-import axios from 'axios';
-import { useCallback, useEffect, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
-import './SearchPage.css';
-import { useDebounce } from '../../hooks/useDebounce';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./SearchPage.css";
+import { useDebounce } from "../../hooks/useDebounce";
 
 const SearchPage = () => {
   const [searchResults, setSearchResults] = useState([]);
@@ -10,59 +10,59 @@ const SearchPage = () => {
 
   const useQuery = () => {
     return new URLSearchParams(useLocation().search);
-  }
+  };
 
   let query = useQuery();
-  const searchTerm = query.get('keyword');
+  const searchTerm = query.get("keyword");
   const debouncedSearchTerm = useDebounce(searchTerm, 1000);
 
   useEffect(() => {
     if (debouncedSearchTerm) {
       fetchTour(debouncedSearchTerm);
     }
-  }, [debouncedSearchTerm, fetchTour])
+  }, [debouncedSearchTerm]);
 
-  const fetchTour = useCallback(async () => {
+  const fetchTour = async () => {
     try {
-      await axios.get(
-        `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?serviceKey=D6HvbqfFj6otDTGY3883h0C51xIplWlMUXEF%2Bl5ZX9DTpTTNODdcI%2F6StO1BbYtjTAtOOKyj25hhnMVj4ASszw%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A&contentTypeId=12&keyword=${debouncedSearchTerm}`
-      ).then((response) => {
-        setSearchResults(response.data.response.body.items.item);
-      });
+      const response = await axios.get(
+        `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?serviceKey=D6HvbqfFj6otDTGY3883h0C51xIplWlMUXEF%2Bl5ZX9DTpTTNODdcI%2F6StO1BbYtjTAtOOKyj25hhnMVj4ASszw%3D%3D&numOfRows=60&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A&contentTypeId=12&keyword=${debouncedSearchTerm}`
+      );
+      setSearchResults(response.data.response.body.items.item);
     } catch (error) {
-      console.log('에러입니다.', error);
+      console.log("에러입니다.", error);
     }
-  }, [debouncedSearchTerm])
+  };
   if (searchResults === null) return null;
 
   if (searchResults && searchResults.length > 0) {
     return (
-      <section className='search_container'>
+      <section className="search_container">
         {searchResults.map((item) => (
-          <div className='tour' key={item.contentid} >
-            <div className='tour_column_img' onClick={() => navigate(`/${item.title}`)}>
-              <img className='tour_img'
-                src={item.firstimage ? item.firstimage : 'images/no_img.jpg'}
-                alt='관광지 이미지'
+          <div className="tour" key={item.contentid}>
+            <div
+              className="tour_column_img"
+              onClick={() => navigate(`/${item.title}`)}
+            >
+              <img
+                className="tour_img"
+                src={item.firstimage ? item.firstimage : "images/no_img.jpg"}
+                alt="관광지 이미지"
               />
+              <h3>{item.title}</h3>
             </div>
           </div>
         ))}
       </section>
-    )
+    );
   } else {
     return (
-      <section className='no_results'>
-        <div className='no_results_text'>
-          <p>
-            찾고자하는 검색어 {debouncedSearchTerm} 관광지가 없습니다.
-          </p>
+      <section className="no_results">
+        <div className="no_results_text">
+          <p>찾고자하는 검색어 {debouncedSearchTerm} 관광지가 없습니다.</p>
         </div>
       </section>
-    )
+    );
   }
+};
 
-
-}
-
-export default SearchPage
+export default SearchPage;
